9163ae8d95e867f2a10f25850c147ae13abb98f3
nicollasdev <contatonicollasfeitosa@gmail.com>
2021-09-20 19:11:32 -0300

    Add example to connect MQTT

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ docker build --no-cache -t rmq-mqtt ./env/rabbit
 
 ### Rodar o docker compose
 docker-compose up -d
+
+### Verificar se o plugin foi instalado
+### > Dentro do container do rabbit
+rabbitmq-plugins list
+
+## Packages
+go get github.com/eclipse/paho.mqtt.golang
--- src/main.go
+++ src/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"time"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
 func main() {
-	
+	var broker = "localhost"
+	var port = 1883
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+	opts.SetClientID("go_mqtt_client")
+	opts.SetUsername("guest")
+	opts.SetPassword("guest")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	fmt.Println("GoLang")
+	sub(client)
+	publish(client)
+}
+
+func publish(client mqtt.Client) {
+  num := 25
+  for i := 0; i < num; i++ {
+    text := fmt.Sprintf("Message %d", i)
+    token := client.Publish("enviador", 1, false, text)
+    test := token.Wait()
+	fmt.Println(test)
+    time.Sleep(time.Millisecond * 500)
+  }
+}
+
+func sub(client mqtt.Client) {
+	topic := "enviador"
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+  	fmt.Printf("Subscribed to topic: %s", topic)
 }
